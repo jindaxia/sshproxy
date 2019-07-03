d9dc51940994fc4a21733d06a3023f4bbf23b02b
jindaxia <jfk_jin@sina.com>
2019-07-03 16:53:32 +0800

    add bastion support

--- sshproxy/main.py
+++ sshproxy/main.py
@@ -8,41 +8,64 @@ from proxy import HTTP, logger
 
 def set_connection():
     port = 10888
-    server = ['10.152.105.137', 22]
-    remote = ['127.0.0.1', 8080]
+    server = ['1.1.1.1', 22]  # server ip and port
+    login = ['root', '111111'] # login user and password
+    bastion = ['1.1.1.1', 22, 'root', '1'] # bastion login
+
+    bastion_channel = build_jump(bastion, server[0], server[1])
+    
     client = paramiko.SSHClient()
     client.load_system_host_keys()
-    client.set_missing_host_key_policy(paramiko.WarningPolicy())
+    client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    
 
-    print("Connecting to ssh host %s:%d ..." % (server[0], server[1]))
     try:
+        print("Connecting to ssh host %s:%d ..." % (server[0], server[1]))
         client.connect(
             server[0],
             server[1],
-            username='root',
-            # key_filename=options.keyfile,
-            # look_for_keys=options.look_for_keys,
-            password='nsfocus',
+            username=login[0],
+            password=login[1],
+            sock= bastion_channel,
         )
     except Exception as e:
         print("*** Failed to connect to %s:%d: %r" % (server[0], server[1], e))
         sys.exit(1)
 
-    print(
-        "Now forwarding remote port %d to %s:%d ..."
-        % (port, remote[0], remote[1])
-    )
-
     setup_yum_proxy(client, port)
+    setup_wget_proxy(client, port)
 
-    # try:
-    #     reverse_forward_tunnel(
-    #         port, remote[0], remote[1], client.get_transport()
-    #     )
-    # except KeyboardInterrupt:
-    #     print("C-c: Port forwarding stopped.")
-    #     sys.exit(0)
+    try:
+        remote = ['127.0.0.1', 8080]
+        print(
+            "Now forwarding remote port %d to %s:%d ..." % (port, remote[0], remote[1])
+        )
+        reverse_forward_tunnel(
+            port, remote[0], remote[1], client.get_transport()
+        )
+    except KeyboardInterrupt:
+        print("C-c: Port forwarding stopped.")
+        sys.exit(0)
 
+def build_jump(bastion, des_server, des_port):
+    bastion_client = paramiko.SSHClient()
+    bastion_client.load_system_host_keys()
+    bastion_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    try:
+        print("*** connecting to bastion %s:%d: %r" % (bastion[0], bastion[1], e))
+        bastion_client.connect(
+            bastion[0],
+            bastion[1],
+            username=bastion[2],
+            password=bastion[3],
+        )
+        bastion_transport = bastion_client.get_transport()
+        dest_addr = (des_server, des_port)
+        src_addr = ('localhost', 22)
+        jumpbox_channel = bastion_transport.open_channel("direct-tcpip", dest_addr, src_addr)
+    except Exception as e:
+        print("*** Failed to connect to bastion %s:%d: %r" % (bastion[0], bastion[1], e))
+        return None
 
 def setup_yum_proxy(client, port):
     stdin, stdout, stderr = client.exec_command('grep proxy= /etc/yum.conf')
@@ -57,6 +80,12 @@ def setup_yum_proxy(client, port):
             stdin, stdout, stderr = client.exec_command('echo "proxy=http://localhost:10888/" >> /etc/yum.conf')
             print(stderr.readlines())
 
+
+def setup_wget_proxy(client, port):
+    stdin, stdout, stderr = client.exec_command("sed -i 's/^#\?http_proxy = .*$/http_proxy = http:\/\/localhost:10888\//g' /etc/wgetrc")
+    if len(stderr.readlines())> 0:
+        print("ERROR set wget proxy ")
+    stdin, stdout, stderr = client.exec_command("sed -i 's/^#\?https_proxy = .*$/https_proxy = http:\/\/localhost:10888\//g' /etc/wgetrc")
 
 def setup_proxy():
     try:
@@ -75,11 +104,10 @@ def setup_proxy():
         print("KeyboardInterrupt Exit")
 
 
-
 if __name__ == '__main__':
-    # thr = threading.Thread(
-    #     target=setup_proxy, args=()
-    # )
-    # thr.setDaemon(True)
-    # thr.start()
+    thr = threading.Thread(
+        target=setup_proxy, args=()
+    )
+    thr.setDaemon(True)
+    thr.start()
     set_connection()
