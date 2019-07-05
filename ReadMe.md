7255b7eb6d70b5a5dede4c74cba6279239f88945
jindaxia <jfk_jin@sina.com>
2019-07-05 16:56:59 +0800

    format python code and add ReadMe

--- ReadMe.md
+++ ReadMe.md
@@ -2,15 +2,34 @@
 
 A TCP proxy for yum，pip or wget on ssh tunnel portfowarding
 
-## features
+## Features
 
 - a simple http(s) proxy
-
 - show request logs
+- auto set yum/wget config file，
 
-- auto set yum/pip config file， with backup/restore
+## Todo
 
-- file transfer？
+- file transfer
+- set pip config file
+- backup/restore yum/wget config file
 
-  
+## Usage
 
+1. Edit the server login and bastion login（if needed）
+
+    ```python
+        server = ['172.20.243.22', 22]  # server ip/port
+        login = ['root', 'Passw0rd'] # login user/password
+        bastion = ['1.1.1.1', 22, 'root', '1'] # bastion login
+    ```
+
+2. run main.py
+
+> python main.py
+
+## Requirement
+
+1. paramiko
+
+> pip install paramiko
--- sshproxy/main.py
+++ sshproxy/main.py
@@ -13,11 +13,10 @@ def set_connection():
     bastion = ['1.1.1.1', 22, 'root', '1'] # bastion login
 
     bastion_channel = build_jump(bastion, server[0], server[1])
-    
+
     client = paramiko.SSHClient()
     client.load_system_host_keys()
     client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-    
 
     try:
         print("Connecting to ssh host %s:%d ..." % (server[0], server[1]))
@@ -26,7 +25,7 @@ def set_connection():
             server[1],
             username=login[0],
             password=login[1],
-            sock= bastion_channel,
+            sock=bastion_channel,
         )
     except Exception as e:
         print("*** Failed to connect to %s:%d: %r" % (server[0], server[1], e))
@@ -38,7 +37,8 @@ def set_connection():
     try:
         remote = ['127.0.0.1', 8080]
         print(
-            "Now forwarding remote port %d to %s:%d ..." % (port, remote[0], remote[1])
+            "Now forwarding remote port %d to %s:%d ..." % (
+                port, remote[0], remote[1])
         )
         reverse_forward_tunnel(
             port, remote[0], remote[1], client.get_transport()
@@ -47,12 +47,14 @@ def set_connection():
         print("C-c: Port forwarding stopped.")
         sys.exit(0)
 
+
 def build_jump(bastion, des_server, des_port):
     bastion_client = paramiko.SSHClient()
     bastion_client.load_system_host_keys()
     bastion_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
     try:
-        print("*** connecting to bastion %s:%d: %r" % (bastion[0], bastion[1], e))
+        print("*** connecting to bastion %s:%d: %r" %
+              (bastion[0], bastion[1], e))
         bastion_client.connect(
             bastion[0],
             bastion[1],
@@ -62,35 +64,43 @@ def build_jump(bastion, des_server, des_port):
         bastion_transport = bastion_client.get_transport()
         dest_addr = (des_server, des_port)
         src_addr = ('localhost', 22)
-        jumpbox_channel = bastion_transport.open_channel("direct-tcpip", dest_addr, src_addr)
+        jumpbox_channel = bastion_transport.open_channel(
+            "direct-tcpip", dest_addr, src_addr)
     except Exception as e:
-        print("*** Failed to connect to bastion %s:%d: %r" % (bastion[0], bastion[1], e))
+        print("*** Failed to connect to bastion %s:%d: %r" %
+              (bastion[0], bastion[1], e))
         return None
+
 
 def setup_yum_proxy(client, port):
     stdin, stdout, stderr = client.exec_command('grep proxy= /etc/yum.conf')
-    if len(stderr.readlines())> 0:
+    if len(stderr.readlines()) > 0:
         print("ERROR execute grep ")
     else:
         result = stdout.readlines()
         if len(result) == 1 and 'proxy=' in result[0]:
-            stdin, stdout, stderr = client.exec_command("sed -i 's/^proxy=.*$/proxy=http:\/\/localhost:10888\//g' /etc/yum.conf")
+            stdin, stdout, stderr = client.exec_command(
+                "sed -i 's/^proxy=.*$/proxy=http:\/\/localhost:10888\//g' /etc/yum.conf")
             print(stderr.readlines())
         else:
-            stdin, stdout, stderr = client.exec_command('echo "proxy=http://localhost:10888/" >> /etc/yum.conf')
+            stdin, stdout, stderr = client.exec_command(
+                'echo "proxy=http://localhost:10888/" >> /etc/yum.conf')
             print(stderr.readlines())
 
 
 def setup_wget_proxy(client, port):
-    stdin, stdout, stderr = client.exec_command("sed -i 's/^#\?http_proxy = .*$/http_proxy = http:\/\/localhost:10888\//g' /etc/wgetrc")
-    if len(stderr.readlines())> 0:
+    stdin, stdout, stderr = client.exec_command(
+        "sed -i 's/^#\?http_proxy = .*$/http_proxy = http:\/\/localhost:10888\//g' /etc/wgetrc")
+    if len(stderr.readlines()) > 0:
         print("ERROR set wget proxy ")
-    stdin, stdout, stderr = client.exec_command("sed -i 's/^#\?https_proxy = .*$/https_proxy = http:\/\/localhost:10888\//g' /etc/wgetrc")
+    stdin, stdout, stderr = client.exec_command(
+        "sed -i 's/^#\?https_proxy = .*$/https_proxy = http:\/\/localhost:10888\//g' /etc/wgetrc")
+
 
 def setup_proxy():
     try:
         logging.basicConfig(level=logging.INFO,
-                        format='%(asctime)s - %(levelname)s - %(funcName)s:%(lineno)d - %(message)s'),
+                            format='%(asctime)s - %(levelname)s - %(funcName)s:%(lineno)d - %(message)s'),
 
         proxy = HTTP(hostname='127.0.0.1',
                      port=int(8080),
